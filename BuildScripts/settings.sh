be85424533fb58271294c1ce8bd8d1d725180eae
Chris <http://chris.copsey.net>
2015-09-28 10:39:17 +0000

    Latest versions of BuildScripts and ModuleFiles.

--- BuildScripts/settings.sh
+++ BuildScripts/settings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ROOT_DIR="/opt/OpenFOAM"
+ROOT_DIR="${HOME}/OpenFOAM"
 ARCHIVES_DIR="$ROOT_DIR/Archives"
 BUILD_DIR="$ROOT_DIR/Build"
-INSTALL_PREFIX="$ROOT_DIR"
+INSTALL_PREFIX="$ROOT_DIR/Dependencies"
 
 CORES=8
 
